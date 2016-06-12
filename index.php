d29222651e31c8a13b115dc78e6a909025ae0cc2
cyrus <827264622@qq.com>
2016-06-12 16:43:30 +0800

    jq案例

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+define('APP_NAME','Home');
+define('APP_PATH','./Home/');
+define('APP_DEBUG',true);
+define('NO_CACHE_RUNTIME',true);
+define('STRIP_RUNTIME_SPACE',false);
+require('ThinkPHP/ThinkPHP.php');
+ ?>
